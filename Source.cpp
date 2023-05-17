5f23f006116b6fdcb2b9d0f774ba528eddadc7d4
AhmedAElrefae <besoaboadel@gmail.com>
2023-05-17 07:56:18 +0300

    made some new things.
    first, i added movement collision to both the ball and the player body and the court walls,
    second, i added motion to the ball with bouncing,
    third, i added a score checker that checks if you put the ball in the basket,
    finally, i did some code formatting and editing.

--- Source.cpp
+++ Source.cpp
@@ -69,16 +69,24 @@ vector<Keyframe> run_keyframes_hands = {
     {0.0f, {0.0f, 0.4f, 2.2f}, {-18.0f, 0.0f, 0.0f}, {1.0f, 1.0f, 1.0f}},
 };
 unsigned int camera_state = 0;
-float player_move_speed = 0.05;
+float prev_time = 0;
+float player_move_speed = 0.0;
 bool keys[256] = { false }; // Array to store the state of each key
-bool pause = false;
 float jump_start_time = 0.0f;
 float run_start_time = 0.0f;
 int frame_count = 0;
-int current_time = 0;
-int previous_time = 0;
 float frame_rate = 0.0f;
 int last_x, last_y;
+bool ball_shot = false;
+float ball_vertical_speed = 20;
+float ball_horizontal_speed = 20;
+float forward_x;
+float forward_z;
+float forward_x_ball;
+float forward_z_ball;
+int screen_width = GetSystemMetrics(SM_CXSCREEN);
+int screen_height = GetSystemMetrics(SM_CYSCREEN);
+int score = 0;
 
 
 const GLfloat light_ambient[] = { 0.0f, 0.0f, 0.0f, 1.0f };
@@ -104,8 +112,8 @@ void special(int, int, int);
 void specialUp(int, int, int);
 void idle();
 void timer(int);
-void interpolateKeyframes(float, const Keyframe&, const Keyframe&, vec3&, vec3&, vec3&);
-void showFrameRate();
+void updateBallPosition();
+void checkBallScore();
 /* Program entry point */
 int main(int argc, char* argv[])
 {
@@ -132,7 +140,6 @@ int main(int argc, char* argv[])
     glutKeyboardFunc(key);
     glutKeyboardUpFunc(keyUp);
     glutSpecialFunc(special);
-    //glutSpecialUpFunc(specialUp);
     glutIdleFunc(idle);
     glutTimerFunc(1000, timer, 0);
 
@@ -236,6 +243,8 @@ void init() {
 /* GLUT callback Handlers */
 void resize(int width, int height)
 {
+    screen_height = height;
+    screen_width = width;
     const float ar = (float)width / (float)height;
     glViewport(0, 0, width, height);
     glMatrixMode(GL_PROJECTION);
@@ -283,7 +292,12 @@ void setCamera(int camera_state) {
 }
 void display()
 {
-    //showFrameRate();
+    // Calculate the time delta since the last frame
+    float current_time = glutGet(GLUT_ELAPSED_TIME) / 1000;
+    float dt = current_time - prev_time;
+    forward_x = sin(player_body.transform.rotation.y * M_PI / 180.0f);
+    forward_z = -cos(player_body.transform.rotation.y * M_PI / 180.0f);
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     // Set up camera
@@ -297,6 +311,26 @@ void display()
     gluLookAt(camera.position[0], camera.position[1], camera.position[2],
         camera.lookat[0], camera.lookat[1], camera.lookat[2], camera.up[0], camera.up[1], camera.up[2]);
 
+    //playerCollision();
+
+    
+    player_body.transform.position.x += forward_x * player_move_speed;
+    player_body.transform.position.z -= forward_z * player_move_speed;
+
+    if (player_body.transform.position.x + 1.5 > 40.0) {
+        player_body.transform.position.x = 38.5;
+    }
+    if (player_body.transform.position.x - 1.5 < -40.0) {
+        player_body.transform.position.x = -38.5;
+    }
+    if (player_body.transform.position.z + 2 > 65.0) {
+        player_body.transform.position.z = 63;
+    }
+    if (player_body.transform.position.z - 2 < -65.0) {
+        player_body.transform.position.z = -63;
+    }
+
+
     // draw the basketball court
     glPushMatrix();
     glColor3f(0.0, 0.6, 0.0); // set the color to green
@@ -307,23 +341,27 @@ void display()
     glPushMatrix();
     glColor3f(0.4, 0.6, 0.0);
     glTranslatef(40.0f, 20.0f, 0.0f);
+    glNormal3f(0.0f, 1.0f, 0.0f);
     glCallList(long_wall);
     glPopMatrix();
     glPushMatrix();
     glColor3f(0.4, 0.6, 0.0);
     glTranslatef(-40.0f, 20.0f, 0.0f);
+    glNormal3f(0.0f, 1.0f, 0.0f);
     glCallList(long_wall);
     glPopMatrix();
     glPushMatrix();
     glColor3f(0.4, 0.6, 0.0);
     glTranslatef(0.0f, 20.0f, 65.0f);
     glRotatef(90.0f, 0.0f, 1.0f, 0.0f);
+    glNormal3f(0.0f, 1.0f, 0.0f);
     glCallList(short_wall);
     glPopMatrix();
     glPushMatrix();
     glColor3f(0.4, 0.6, 0.0);
     glTranslatef(0.0f, 20.0f, -65.0f);
     glRotatef(90.0f, 0.0f, 1.0f, 0.0f);
+    glNormal3f(0.0f, 1.0f, 0.0f);
     glCallList(short_wall);
     glPopMatrix();
 
@@ -334,8 +372,8 @@ void display()
     glPopMatrix();
 
     // draw basketball panels
-    glPushMatrix();
     glColor3f(1.0, 0.0, 0.0);
+    glPushMatrix();
     basketball_panel.transform.position.z = -59.5;
     glTranslated(basketball_panel.transform.position.x, 0.0, basketball_panel.transform.position.z);
     glCallList(basketball_panel_list);
@@ -353,10 +391,64 @@ void display()
     glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
     glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
     glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
-    glTranslatef(0.0f, 20.0f, -20.0f);
+    checkBallScore();
+    if (!ball_shot) {
+        ball.transform.position = player_body.transform.position;
+        ball.transform.rotation = player_body.transform.rotation;
+    }
+    
+    glTranslatef(ball.transform.position.x, ball.transform.position.y, ball.transform.position.z);
+    glRotatef(ball.transform.rotation.x, 1, 0, 0);
+    glRotatef(ball.transform.rotation.y, 0, 1, 0);
+    glRotatef(ball.transform.rotation.z, 0, 0, 1);
+    glScalef(ball.transform.scale.x, ball.transform.scale.y, ball.transform.scale.z);
     ball.drawShape();
 
+    // Set the raster position for the text
+    glMatrixMode(GL_PROJECTION);
+    glPushMatrix();
+    glLoadIdentity();
+    gluOrtho2D(0, screen_width, 0, screen_height);
+    glMatrixMode(GL_MODELVIEW);
+    glPushMatrix();
+    glLoadIdentity();
+    glRasterPos2i(0, 10); // position of the text
+
+    string welcome = "Hello, player!"; 
+    int x = 10; // initial x position
+    int y = screen_height - 15; // initial y position
+    for (char c : welcome) {
+        glRasterPos2i(x, y); // set the position for this character
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, c);
+        x += glutBitmapWidth(GLUT_BITMAP_HELVETICA_18, c) + 4; // advance x position
+    }
+    string power = "power:" + to_string(ball_horizontal_speed) + " vertical power:" + to_string(ball_vertical_speed); 
+    x = 10;
+    y = 10; 
+    for (char c : power) {
+        glRasterPos2i(x, y);
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, c);
+        x += glutBitmapWidth(GLUT_BITMAP_HELVETICA_18, c) + 1; // advance x position
+    }
+    glRasterPos2i(0, 10); // position of the text
+
+    string scorestr = "score: " + to_string(score);
+    x = 10; // initial x position
+    y = screen_height - 35; // initial y position
+    for (char c : scorestr) {
+        glRasterPos2i(x, y); // set the position for this character
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, c);
+        x += glutBitmapWidth(GLUT_BITMAP_HELVETICA_18, c) + 4; // advance x position
+    }
+
+    // Restore the matrices
+    glPopMatrix();
+    glMatrixMode(GL_PROJECTION);
+    glPopMatrix();
+    glMatrixMode(GL_MODELVIEW);
+
     glutSwapBuffers();
+    prev_time = current_time;
 }
 void mouse(int button, int state, int x, int y) {
     if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
@@ -393,6 +485,16 @@ void key(unsigned char key, int x, int y)
             jump_start_time = glutGet(GLUT_ELAPSED_TIME) / 1000.0f;
         }
         break;
+    case 'x':
+        ball_shot = false;
+        ball_vertical_speed = 20;
+        ball_horizontal_speed = 20;
+        break;
+    case 13:
+        ball_shot = true;
+        forward_x_ball = forward_x;
+        forward_z_ball = forward_z;
+        break;
     case 'C':
     case 'c':
         camera_state = ++camera_state % 4;
@@ -409,11 +511,41 @@ void key(unsigned char key, int x, int y)
 void keyUp(unsigned char key, int x, int y)
 {
     keys[key] = false; // Set the state of the released key to false
+    player_move_speed = 0.0;
 }
 void special(int key, int x, int y)
 {
     switch (key)
     {
+    case GLUT_KEY_UP:
+        // Handle the up arrow key press
+        ball_vertical_speed += 1;
+        if (ball_vertical_speed > 50) {
+            ball_vertical_speed = 50;
+        }
+        break;
+    case GLUT_KEY_DOWN:
+        // Handle the down arrow key press
+        ball_vertical_speed -= 1;
+        if (ball_vertical_speed < 0) {
+            ball_vertical_speed = 0;
+        }
+        break;
+    case GLUT_KEY_LEFT:
+        // Handle the left arrow key press
+        ball_horizontal_speed += 1;
+        if (ball_horizontal_speed > 50) {
+            ball_horizontal_speed = 50;
+        }
+        break;
+    case GLUT_KEY_RIGHT:
+        // Handle the right arrow key press
+        ball_horizontal_speed -= 1;
+        if (ball_horizontal_speed < 0) {
+            ball_horizontal_speed = 0;
+        }
+        break;
+        
         break;
     }
 }
@@ -426,31 +558,28 @@ void specialUp(int key, int x, int y)
 }
 void idle()
 {
-    float camera_free_speed = 0.01;
-    float forward_x = sin(player_body.transform.rotation.y * M_PI / 180.0f);
-    float forward_z = -cos(player_body.transform.rotation.y * M_PI / 180.0f);
+    float camera_free_speed = 0.05;
+
     // Check the state of each key and take appropriate action
     if (keys['w']) {
         if (!animate.run) {
             animate.run = true;
             run_start_time = glutGet(GLUT_ELAPSED_TIME) / 1000;
         }
-        player_body.transform.position.x += forward_x * player_move_speed;
-        player_body.transform.position.z -= forward_z * player_move_speed;
+        player_move_speed = 0.05;
     }
     if (keys['s']) {
         if (!animate.run) {
             animate.run = true;
             run_start_time = glutGet(GLUT_ELAPSED_TIME) / 1000;
         }
-        player_body.transform.position.x -= forward_x * player_move_speed;
-        player_body.transform.position.z += forward_z * player_move_speed;
+        player_move_speed = -0.05;
     }
     if (keys['a']) {
-        player_body.transform.rotation.y += player_move_speed + 0.15;
+        player_body.transform.rotation.y += player_move_speed + 0.25;
     }
     if (keys['d']) {
-        player_body.transform.rotation.y -= player_move_speed + 0.15;
+        player_body.transform.rotation.y -= player_move_speed + 0.25;
     }
     if (keys['i']) {
         camera.position[1] += camera_free_speed;
@@ -480,7 +609,8 @@ void idle()
 }
 void timer(int value) {
     glutPostRedisplay();
-
+    
+    updateBallPosition();
     // Update player position based on animation
     if (animate.jump) {
         float time_since_jump_start = glutGet(GLUT_ELAPSED_TIME) / 1000.0f - jump_start_time;
@@ -499,6 +629,7 @@ void timer(int value) {
             float t = (time_since_jump_start - jump_keyframes[i].time) / (jump_keyframes[i + 1].time - jump_keyframes[i].time);
             player_body.transform.position.y = jump_keyframes[i].position.y * (1 - t) + jump_keyframes[i + 1].position.y * t;
             player_body.transform.scale.y = jump_keyframes[i].scale.y * (1 - t) + jump_keyframes[i + 1].scale.y * t;
+            ball.transform.scale.y = jump_keyframes[i].scale.y * (1 - t) + jump_keyframes[i + 1].scale.y * t;
         }
     }
 
@@ -530,25 +661,25 @@ void timer(int value) {
 
         }
         // right hand
-        if (time_since_run_start >= run_keyframes_hands[run_keyframes_hands.size() - 1].time) {
-            // Animation is finished
-            animate.run = false;
-            player_body.childModels[2].transform.rotation.x = 0.0f;
-            player_body.childModels[2].transform.position.z = 0.0f;
-            player_body.childModels[2].transform.position.y = 0.0f;
-        }
-        else {
-            // Find current keyframe
-            int i = 0;
-            while (i < run_keyframes_hands.size() - 1 && time_since_run_start >= run_keyframes_hands[i + 1].time) {
-                i++;
-            }
-            // Interpolate between keyframes
-            float t = (time_since_run_start - run_keyframes_hands[i].time) / (run_keyframes_hands[i + 1].time - run_keyframes_hands[i].time);
-            player_body.childModels[2].transform.rotation.x = (-run_keyframes_hands[i].rotation.x) * (1 - t) + (-run_keyframes_hands[i + 1].rotation.x) * t;
-            player_body.childModels[2].transform.position.z = (-run_keyframes_hands[i].position.z) * (1 - t) + (-run_keyframes_hands[i + 1].position.z) * t;
-            player_body.childModels[2].transform.position.y = run_keyframes_hands[i].position.y * (1 - t) + run_keyframes_hands[i + 1].position.y * t;
-        }
+        //if (time_since_run_start >= run_keyframes_hands[run_keyframes_hands.size() - 1].time) {
+        //    // Animation is finished
+        //    animate.run = false;
+        //    player_body.childModels[2].transform.rotation.x = 0.0f;
+        //    player_body.childModels[2].transform.position.z = 0.0f;
+        //    player_body.childModels[2].transform.position.y = 0.0f;
+        //}
+        //else {
+        //    // Find current keyframe
+        //    int i = 0;
+        //    while (i < run_keyframes_hands.size() - 1 && time_since_run_start >= run_keyframes_hands[i + 1].time) {
+        //        i++;
+        //    }
+        //    // Interpolate between keyframes
+        //    float t = (time_since_run_start - run_keyframes_hands[i].time) / (run_keyframes_hands[i + 1].time - run_keyframes_hands[i].time);
+        //    player_body.childModels[2].transform.rotation.x = (-run_keyframes_hands[i].rotation.x) * (1 - t) + (-run_keyframes_hands[i + 1].rotation.x) * t;
+        //    player_body.childModels[2].transform.position.z = (-run_keyframes_hands[i].position.z) * (1 - t) + (-run_keyframes_hands[i + 1].position.z) * t;
+        //    player_body.childModels[2].transform.position.y = run_keyframes_hands[i].position.y * (1 - t) + run_keyframes_hands[i + 1].position.y * t;
+        //}
 
         // left leg
         if (time_since_run_start >= run_keyframes_legs[run_keyframes_legs.size() - 1].time) {
@@ -595,21 +726,71 @@ void timer(int value) {
     }
     glutTimerFunc(REFRESH_RATE, timer, 0);
 }
-void showFrameRate() {
-    // Increment the frame count
-    frame_count++;
-
-    // Get the current time in milliseconds
-    current_time = glutGet(GLUT_ELAPSED_TIME);
-
-    // Calculate the time elapsed since the last frame
-    int elapsed_time = current_time - previous_time;
-
-    // Calculate the frame rate
-    if (elapsed_time > 1000) {
-        frame_rate = frame_count / (elapsed_time / 1000.0f);
-        previous_time = current_time;
-        frame_count = 0;
-        cout << "frame rate is : " << frame_rate << endl;
+void updateBallPosition() {
+    if (ball_shot) {
+        ball_vertical_speed -= GRAVITY * 0.03;
+        vec3 new_pos = ball.transform.position;
+        new_pos.x += forward_x_ball * ball_horizontal_speed * 0.03;
+        new_pos.y = ball.transform.position.y + ball_vertical_speed * 0.03f;
+        new_pos.z -= forward_z_ball * ball_horizontal_speed * 0.03;
+        if (new_pos.y + 3.1 <= 0.0) {
+            ball_vertical_speed *= -0.8;
+            new_pos.y = -3.1;
+        }
+        if (new_pos.x > 40.0) {
+            ball_vertical_speed *= -0.5;
+            ball_horizontal_speed *= -0.5;
+            new_pos.x = 38;
+        }
+        if (new_pos.x - 2.5 < -40.0) {
+            ball_vertical_speed *= -0.5;
+            ball_horizontal_speed *= -0.5;
+            new_pos.x = -37.5;
+        }
+        if (new_pos.z + 1 > 58.9 && new_pos.x + 1 < 6 && new_pos.x + 1 > -3 && new_pos.y + 1 > 10 && new_pos.y + 1 < 15) {
+            ball_vertical_speed *= -0.5;
+            ball_horizontal_speed *= -0.5;
+            new_pos.z = 57.9;
+        }
+        else if (new_pos.z + 1 > 65.0) {
+            ball_vertical_speed *= -0.5;
+            ball_horizontal_speed *= -0.5;
+            new_pos.z = 64;
+        }
+        if (new_pos.z - 1 < -58.9 && new_pos.x + 1 > -6 && new_pos.x + 1 < 3 && new_pos.y + 1 > 10 && new_pos.y + 1 < 15) {
+            ball_vertical_speed *= -0.5;
+            ball_horizontal_speed *= -0.5;
+            new_pos.z = -57.9;
+        }
+        else if (new_pos.z - 1 < -65.0) {
+            ball_vertical_speed *= -0.5;
+            ball_horizontal_speed *= -0.5;
+            new_pos.z = -64;
+        }
+        
+        ball_horizontal_speed *= 0.995;
+        ball.transform.position = new_pos;
+    }
+    if (ball_vertical_speed < 0.5 && ball_vertical_speed > -0.5 &&
+        ball_horizontal_speed < 0.8 && ball_horizontal_speed > -0.8) {
+        ball_shot = false;
+        ball_vertical_speed = 20;
+        ball_horizontal_speed = 20;
+    }
+}
+void checkBallScore() {
+    static bool ball_in_basket = false;
+    vec3 ball_position = ball.transform.position;
+    vec3 basket_position (1.4,9,58);
+    float distance = sqrt(pow(ball_position.x - basket_position.x, 2) + pow(ball_position.y - basket_position.y, 2) + pow(ball_position.z - basket_position.z, 2));
+    if (distance < 2.0 && !ball_in_basket) {
+        // The ball fell in the basket for the first time
+        cout << "scooooooore" << endl;
+        score++;
+        ball_in_basket = true;
+    }
+    else if (distance >= 2.0) {
+        // The ball is not in the basket
+        ball_in_basket = false;
     }
 }
